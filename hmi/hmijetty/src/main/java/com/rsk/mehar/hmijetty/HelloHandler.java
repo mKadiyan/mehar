539f2e3565e3847861e4863128bf94e69729cc6e
ravinder kadiyan <ravinder.kadiyan@gmail.com>
2014-11-09 21:05:21 +0530

    added sample jetty server

---
+++ hmi/hmijetty/src/main/java/com/rsk/mehar/hmijetty/HelloHandler.java
@@ -0,0 +1,30 @@
+package com.rsk.mehar.hmijetty;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.LogSF;
+import org.apache.log4j.Logger;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+public class HelloHandler extends AbstractHandler
+{
+    private static final Logger LOGGER = Logger.getLogger(HelloHandler.class);
+    
+    public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ServletException
+    {
+        if (LOGGER.isInfoEnabled())
+        {
+            LogSF.info(LOGGER, "Request came :target = {} , baseRequest={} ,request={}", target, baseRequest, request);
+        }
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        baseRequest.setHandled(true);
+        response.getWriter().println("<h1>Hello RSK</h1>");
+    }
+}
---
+++ hmi/hmijetty/src/main/java/com/rsk/mehar/hmijetty/SimplestServer.java
@@ -0,0 +1,31 @@
+package com.rsk.mehar.hmijetty;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.eclipse.jetty.server.Server;
+
+public class SimplestServer
+{
+    private static final Logger LOGGER = Logger.getLogger(SimplestServer.class);
+    
+    static
+    {
+        BasicConfigurator.configure();
+        Logger.getRootLogger().setLevel(Level.INFO);
+    }
+    
+    public static void main(String[] args) throws Exception
+    {
+        if (LOGGER.isInfoEnabled())
+            LOGGER.info("Starting server");
+        Server server = new Server(8080);
+        server.setHandler(new HelloHandler());
+        if (LOGGER.isInfoEnabled())
+            LOGGER.info("Handler added ");
+        server.start();
+        if (LOGGER.isInfoEnabled())
+            LOGGER.info("Server started");
+        server.join();
+    }
+}
