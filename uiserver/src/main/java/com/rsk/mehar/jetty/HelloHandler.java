fcebde237d95f87088a6570cbf54490863c0bcbd
ravinder kadiyan <ravinder.kadiyan@gmail.com>
2014-11-13 23:58:56 +0530

    maojor structure chnage

---
+++ uiserver/src/main/java/com/rsk/mehar/jetty/HelloHandler.java
@@ -0,0 +1,32 @@
+package com.rsk.mehar.jetty;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.LogSF;
+import org.apache.log4j.Logger;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+public class HelloHandler extends AbstractHandler
+{
+    static int index = 0;
+    private static final Logger LOGGER = Logger.getLogger(HelloHandler.class);
+    
+    public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ServletException
+    {
+        System.err.println("index is : " + index++);
+        if (LOGGER.isInfoEnabled())
+        {
+            LogSF.info(LOGGER, "Request came :target = {} , baseRequest={} ,request={}", target, baseRequest, request);
+        }
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        baseRequest.setHandled(true);
+        response.getWriter().println("<h1>Hello RSK</h1>");
+    }
+}
---
+++ uiserver/src/main/java/com/rsk/mehar/jetty/SimplestServer.java
@@ -0,0 +1,38 @@
+package com.rsk.mehar.jetty;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.eclipse.jetty.server.Server;
+
+public class SimplestServer
+{
+    private static final Logger LOGGER = Logger.getLogger(SimplestServer.class);
+    
+    static
+    {
+        BasicConfigurator.configure();
+        Logger.getRootLogger().setLevel(Level.INFO);
+    }
+    
+    public static void main(String[] args) throws Exception
+    {
+        SimplestServer simplestServer = new SimplestServer();
+        simplestServer.startServer();
+    }
+    
+    public void startServer() throws Exception
+    {
+        if (LOGGER.isInfoEnabled())
+            LOGGER.info("Starting server");
+        Server server = new Server(8080);
+        server.setHandler(new HelloHandler());
+        if (LOGGER.isInfoEnabled())
+            LOGGER.info("Handler added ");
+        server.start();
+        if (LOGGER.isInfoEnabled())
+            LOGGER.info("Server started");
+        server.join();
+        
+    }
+}
