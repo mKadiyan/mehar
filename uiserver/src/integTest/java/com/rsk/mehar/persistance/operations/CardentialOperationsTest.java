eb6a5315ece12dfa6f1d4327c8f5e1da93b9c2e0
ravinder kadiyan <ravinder.kadiyan@gmail.com>
2014-12-07 22:38:46 +0530

    uiserver: database integration test cases added

--- uiserver/src/integTest/java/com/rsk/mehar/persistance/operations/CardentialOperationsTest.java
+++ uiserver/src/integTest/java/com/rsk/mehar/persistance/operations/CardentialOperationsTest.java
@@ -6,13 +6,19 @@
 
 package com.rsk.mehar.persistance.operations;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.rsk.mehar.persistance.exception.DuplicateUserException;
@@ -23,6 +29,13 @@ public class CardentialOperationsTest
 {
     private CardentialOperations cardentialOperations;
     private List<String> tempUsers = new ArrayList<String>();
+    
+    @BeforeClass
+    public static void init()
+    {
+        BasicConfigurator.configure();
+        Logger.getRootLogger().setLevel(Level.INFO);
+    }
     
     @Before
     public void setUp() throws Exception
@@ -39,6 +52,34 @@ public class CardentialOperationsTest
         assertTrue(cardentialOperations.isUserExist("test@gmail.com"));
     }
     
+    @Test(expected = DuplicateUserException.class)
+    public void storeCardentialTwice_secondAdditionShouldThrowDuplicateUserException() throws DuplicateUserException, InvalidUserException
+    {
+        String emailId = "test@gmail.com";
+        cardentialOperations.storeCardentials(emailId, "password");
+        assertTrue(cardentialOperations.isUserExist("test@gmail.com"));
+        tempUsers.add(emailId);
+        cardentialOperations.storeCardentials(emailId, "password");
+    }
+    
+    @Test
+    public void checkOfInvalidUser_userShouldNotExist()
+    {
+        assertFalse(cardentialOperations.isUserExist("idontexist@gmail.com"));
+    }
+    
+    @Test
+    public void deleteCardential_cardentialShouldGetDeleted() throws DuplicateUserException, InvalidUserException
+    {
+        String emailId = "test@gmail.com";
+        cardentialOperations.storeCardentials(emailId, "password");
+        assertTrue(cardentialOperations.isUserExist("test@gmail.com"));
+        
+        cardentialOperations.deleteCardentials(emailId);
+        assertFalse(cardentialOperations.isUserExist("test@gmail.com"));
+        
+    }
+    
     @After
     public void tearDown() throws Exception
     {
@@ -46,6 +87,11 @@ public class CardentialOperationsTest
         {
             cardentialOperations.deleteCardentials(emailId);
         }
+    }
+    
+    @AfterClass
+    public static void shutdown()
+    {
         HibernateUtil.shutdown();
     }
     
--- uiserver/src/main/java/com/rsk/mehar/persistance/operations/CardentialOperations.java
+++ uiserver/src/main/java/com/rsk/mehar/persistance/operations/CardentialOperations.java
@@ -8,6 +8,7 @@ package com.rsk.mehar.persistance.operations;
 
 import java.util.Calendar;
 
+import org.apache.log4j.Logger;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -19,6 +20,7 @@ import com.rsk.mehar.persistance.pojo.Cardential;
 
 public class CardentialOperations
 {
+    private static Logger logger = Logger.getLogger(CardentialOperations.class);
     private SessionFactory sessionFactory;
     
     public CardentialOperations(SessionFactory sessionFactory)
@@ -30,13 +32,13 @@ public class CardentialOperations
     {
         Session session = sessionFactory.openSession();
         session.beginTransaction();
-        Object obj = session.load(Cardential.class, emailId);
         try
         {
-            if (obj == null)
+            if (!checkIfUserExist(session, emailId))
             {
                 Cardential cardential = new Cardential(emailId, password, Calendar.getInstance().getTime(), null, null);
                 session.save(cardential);
+                session.getTransaction().commit();
             }
             else
             {
@@ -45,7 +47,6 @@ public class CardentialOperations
         }
         finally
         {
-            session.getTransaction().commit();
             session.close();
         }
     }
@@ -54,18 +55,25 @@ public class CardentialOperations
     {
         Session session = sessionFactory.openSession();
         session.beginTransaction();
-        Object obj = session.load(Cardential.class, emailId);
         try
         {
-            if (obj != null)
-                return true;
+            return checkIfUserExist(session, emailId);
         }
         finally
         {
             session.close();
         }
-        return false;
         
+    }
+    
+    private boolean checkIfUserExist(Session session, String emailId)
+    {
+        Query q = session.createQuery("select count(*) from Cardential where email = :emailId");
+        q.setParameter("emailId", emailId);
+        Long uniqueResult = (Long) q.uniqueResult();
+        if (uniqueResult == null || uniqueResult == 0)
+            return false;
+        return true;
     }
     
     public void checkCardentials(String emailId, String password) throws InvalidUserException, InvalidCardentialException
@@ -85,7 +93,8 @@ public class CardentialOperations
             
             if (result > 0)
             {
-                System.out.println("Expensive products was removed");
+                if (logger.isInfoEnabled())
+                    logger.info("Cardential successfully removed : " + emailId);
             }
             else
                 throw new InvalidUserException("user doesnt exist : " + emailId);
