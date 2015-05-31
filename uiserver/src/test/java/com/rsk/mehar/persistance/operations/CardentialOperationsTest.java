32d83fe9ae9a74a0166f290b37ae4b7706f6e9d7
Ravinder Kadiyan <ravinder.kadiyan@gmail.com>
2015-05-31 23:26:30 +0530

    improve test structure

---
+++ uiserver/src/test/java/com/rsk/mehar/persistance/operations/CardentialOperationsTest.java
@@ -0,0 +1,17 @@
+package com.rsk.mehar.persistance.operations;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ravinder on 31-05-2015.
+ */
+public class CardentialOperationsTest {
+
+    @Test
+    public void test()
+    {
+        assertTrue(true);
+    }
+}
