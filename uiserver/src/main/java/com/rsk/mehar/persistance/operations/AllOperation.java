0ed91d73225509c9b1b78d0c3b0b3c0b7960e72c
Ravinder Kadiyan <ravinder.kadiyan@gmail.com>
2015-06-09 23:17:51 +0530

    fix checkstyle warnings

--- uiserver/src/main/java/com/rsk/mehar/persistance/operations/AllOperation.java
+++ uiserver/src/main/java/com/rsk/mehar/persistance/operations/AllOperation.java
@@ -1,43 +1,46 @@
 package com.rsk.mehar.persistance.operations;
 
-import com.rsk.mehar.persistance.exception.InvalidCardentialException;
+import org.apache.log4j.LogSF;
+import org.apache.log4j.Logger;
+
 import com.rsk.mehar.persistance.exception.InvalidUserException;
 import com.rsk.mehar.persistance.pojo.User;
 import com.rsk.mehar.persistance.util.HibernateUtil;
-import org.apache.log4j.LogSF;
-import org.apache.log4j.Logger;
 
 /**
  * Created by ravinder on 04-06-2015.
  */
-public class AllOperation {
-
-    UserOperations userOperations ;
+public class AllOperation
+{
+    
+    UserOperations userOperations;
     private static Logger logger = Logger.getLogger(UserOperations.class);
+    
     public AllOperation()
     {
         userOperations = new UserOperations(HibernateUtil.getSessionFactory());
     }
-
+    
     public boolean isUserExist(String emailId)
     {
         return userOperations.isUserExist(emailId);
     }
-
+    
     public boolean isValidCardentials(String emailId, String password)
     {
-        return userOperations.isValidCardentials(emailId,password);
+        return userOperations.isValidCardentials(emailId, password);
     }
-
+    
     public User getUser(String emailId)
     {
-        User user=null;
+        User user = null;
         try
         {
-           return  userOperations.getUser(emailId);
+            return userOperations.getUser(emailId);
         }
-        catch (InvalidUserException e) {
-            LogSF.warn(logger,"User with emailId : {} does not exist ",emailId);
+        catch (InvalidUserException e)
+        {
+            LogSF.warn(logger, "User with emailId : {} does not exist ", emailId);
         }
         return user;
     }
